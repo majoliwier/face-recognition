39a0fbfa985a8897a24ec07eca597e70b49af2f2
dianulka <diana332m@gmail.com>
2025-06-02 11:23:18 +0200

    main temp and alc

--- main/main.c
+++ main/main.c
@@ -33,57 +33,54 @@ void mlx90614_task(void *pvParameters)
     esp_err_t ret;
     float ambient_temp, object_temp;
     uint8_t flags;
-    
+
     ret = mlx90614_init(&sensor);
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "Inicjalizacja MLX90614 nie powiodła się!");
         vTaskDelete(NULL);
         return;
     }
-    
-    ret = mlx90614_set_emissivity(&sensor, 0.98, false);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Ustawienie emisyjności nie powiodło się!");
-    }
-    
-    ret = mlx90614_set_measured_parameters(&sensor, IIR_FILTER_4, FIR_FILTER_64);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Ustawienie parametrów filtrów nie powiodło się!");
-    }
-    
+
+    mlx90614_set_emissivity(&sensor, 0.98, false);
+    mlx90614_set_measured_parameters(&sensor, IIR_FILTER_4, FIR_FILTER_64);
+
+    char msg_ambient[32];
+    char msg_object[32];
+
     while (1) {
-        ret = mlx90614_get_ambient_temp(&sensor, &ambient_temp);
-        if (ret == ESP_OK) {
+        if (mlx90614_get_ambient_temp(&sensor, &ambient_temp) == ESP_OK) {
             ESP_LOGI(TAG, "Temperatura otoczenia: %.2f°C", ambient_temp);
-        } else {
-            ESP_LOGE(TAG, "Błąd odczytu temperatury otoczenia");
+            // snprintf(msg_ambient, sizeof(msg_ambient), "%.2f", ambient_temp);
+            // if (mqtt_client != NULL) {
+            //     esp_mqtt_client_publish(mqtt_client, "sensor/temp_otoczenia", msg_ambient, 0, 1, 0);
+            // }
         }
-        
-        ret = mlx90614_get_object_temp(&sensor, &object_temp);
-        if (ret == ESP_OK) {
+
+        if (mlx90614_get_object_temp(&sensor, &object_temp) == ESP_OK) {
             ESP_LOGI(TAG, "Temperatura obiektu: %.2f°C", object_temp);
-        } else {
-            ESP_LOGE(TAG, "Błąd odczytu temperatury obiektu");
+           if (object_temp > 30.0f) {
+                snprintf(msg_object, sizeof(msg_object), "%.2f", object_temp);
+
+                if (mqtt_client != NULL) {
+                    ESP_LOGI(TAG, "Wysyłanie temperatury przez MQTT: %s", msg_object);
+                    esp_mqtt_client_publish(mqtt_client, "sensor/temperatura", msg_object, 0, 1, 0);
+                }
+            }
         }
-        
-        ret = mlx90614_read_flags(&sensor, &flags);
-        if (ret == ESP_OK && flags != 0) {
+
+        if (mlx90614_read_flags(&sensor, &flags) == ESP_OK && flags != 0) {
             ESP_LOGW(TAG, "Flagi czujnika: 0x%02X", flags);
         }
-        
-        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        vTaskDelay(pdMS_TO_TICKS(8000));
     }
 }
-
-
-
 
 mq3_config_t mq3 = {
     .adc_channel = MQ3_ADC_CHANNEL,
     .threshold = MQ3_THRESHOLD
 };
 esp_mqtt_client_handle_t mqtt_client = NULL;
-
 
 void mq3_task(void *pvParameters)
 {
@@ -99,11 +96,10 @@ void mq3_task(void *pvParameters)
 
         snprintf(msg, sizeof(msg), "%d", value);
 
-        snprintf(msg2, sizeof(msg2), "%d", 300);
         if (mqtt_client != NULL) {
             ESP_LOGI("Mq3", "mqtt poszło");
             esp_mqtt_client_publish(mqtt_client, "sensor/alkohol", msg, 0, 1, 0);
-            esp_mqtt_client_publish(mqtt_client, "sensor/temperatura", msg2, 0, 1, 0);
+           
         }
 
         vTaskDelay(pdMS_TO_TICKS(8000));
@@ -139,5 +135,7 @@ void app_main(void)
     // xTaskCreate(mlx90614_task, "mlx90614_task", 4096, NULL, 5, NULL);
     mqtt_app_start();
     xTaskCreate(mq3_task, "mq3_task", 2048, NULL, 5, NULL);
+    xTaskCreate(mlx90614_task, "mlx90614_task", 4096, NULL, 5, NULL);
+
 }
 
